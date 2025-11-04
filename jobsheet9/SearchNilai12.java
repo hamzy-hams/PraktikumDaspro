5fcb045bc18eb7e7e5392f8a0439eb801af9a963
hamzyhams <nexthamas95@gmail.com>
2025-11-04 11:40:28 +0700

    Update SearchNilai12.java
    modifikasi

--- jobsheet9/SearchNilai12.java
+++ jobsheet9/SearchNilai12.java
@@ -1,17 +1,36 @@
+import java.util.Scanner;
+
 public class SearchNilai12 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        int jumlahMurid;
+        int cariNilai;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Masukkan jumlah murid yang akan dicatat nilainya:");
+        jumlahMurid = sc.nextInt();
+        System.out.println("Masukkan nilai yang akan dicari: ");
+        cariNilai = sc.nextInt();
+
+        int[] arrNilai = new int[jumlahMurid];
+        for (int i = 0; i < jumlahMurid; i++) {
+            System.out.println("Masukkan nilai murid: ");
+            arrNilai[i] = sc.nextInt();
+        }
+        
+        int hasil = -1;
 
         for (int i = 0; i < arrNilai.length; i++){
-            if (key == arrNilai[i]){
+            if (cariNilai == arrNilai[i]){
                 hasil = i;
                 break;
             }
         }
         System.out.println();
-        System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
+        if (hasil == -1) {
+            System.out.println("Nilai tidak ditemukan");
+        } else {
+        System.out.println("Nilai "+ cariNilai +" ketemu di indeks ke-"+hasil);
+        } 
         System.out.println();
     }
 }
